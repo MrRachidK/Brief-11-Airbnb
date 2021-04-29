295647ea3bc813470d7c50e66a130fba51f77e88
MrRachidK <rachid.karbiche.pro@gmail.com>
2021-04-29 12:16:46 +0200

    First cleaning of the data

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Brief-11-Airbnb
+# Brief-12-Airbnb
+
+.
+├── data
+│   ├── 01_raw
+│   │   ├── calendar.csv
+│   │   ├── listings.csv
+│   │   ├── neighbourhoods.csv
+│   │   ├── neighbourhoods.geojson
+│   │   └── reviews.csv
+│   └── 02_intermediate
+│       ├── cleaned_calendar.csv
+│       ├── cleaned_listings.csv
+│       └── cleaned_reviews.csv
+├── notebook
+├── src
+│   ├── __init__.py
+│   ├── data.analyze.py
+│   └── functions.py
+├── .gitignore
+└── README.md
+
+Le brief porte sur l'analyse de la donnée fournie par Airbnb et de sa visualisation à partir d'indicateurs définis. Le but est d'utiliser un outil de visualisation de données et de mettre en évidence un dashboard répertoriant les différentes visualisations liées aux indicateurs recherchés.
--- src/data_analyze.py
+++ src/data_analyze.py
@@ -11,9 +11,9 @@ pd.set_option('display.max_columns', None)
 
 # 1. Import of the datas we need to analyze
 
-calendar_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/calendar.csv").sample(10000)
-listings_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/listings.csv").sample(10000)
-neighbourhoods_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/neighbourhoods.csv")
+calendar_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/calendar.csv")
+listings_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/listings.csv")
+reviews_data = pd.read_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/01_raw/reviews.csv")
 
 print(calendar_data.head())
 print("Il y a {} lignes et {} colonnes dans la base de données {}".format(calendar_data.shape[0], calendar_data.shape[1], namestr(calendar_data, globals())))
@@ -25,8 +25,8 @@ print("Il y a {} lignes et {} colonnes dans la base de données {}".format(listi
 
 separator()
 
-print(neighbourhoods_data.head())
-print("Il y a {} lignes et {} colonnes dans la base de données {}".format(neighbourhoods_data.shape[0], neighbourhoods_data.shape[1], namestr(neighbourhoods_data, globals())))
+print(reviews_data.head())
+print("Il y a {} lignes et {} colonnes dans la base de données {}".format(reviews_data.shape[0], reviews_data.shape[1], namestr(reviews_data, globals())))
 
 separator()
 
@@ -34,24 +34,23 @@ separator()
 
 # a. calendar_data
 
-key_cols_calendar_data = ['date', 'price', 'adjusted_price', 'minimum_nights', 'maximum_nights']
-calendar_data = calendar_data[key_cols_calendar_data].sample(10000)
+key_cols_calendar_data = ['listing_id', 'date', 'available', 'price', 'minimum_nights', 'maximum_nights']
+calendar_data = calendar_data[key_cols_calendar_data]
 
 # b. listings_data
 
-key_cols_listings_data = ['id', 'host_location', 'latitude', 'longitude', 'property_type', 'room_type', 'accommodates', 'bathrooms_text', 'bedrooms', 'beds', 'price', 'minimum_nights', 'maximum_nights', 'review_scores_rating', 'reviews_per_month']
-listings_data = listings_data[key_cols_listings_data].sample(10000)
+key_cols_listings_data = ['id', 'host_id', 'host_name', 'host_is_superhost', 'host_listings_count', 'neighbourhood_cleansed', 'latitude', 'longitude', 'property_type', 'room_type', 'accommodates', 'bathrooms_text', 'bedrooms', 'beds', 'price', 'minimum_nights', 'maximum_nights', 'has_availability', 'availability_365', 'number_of_reviews', 'review_scores_rating', 'reviews_per_month']
+listings_data = listings_data[key_cols_listings_data]
 
-# c. neighbourhoods_data
+# c. reviews_data
 
-neighbourhoods_data = neighbourhoods_data['neighbourhood']
+key_cols_reviews_data = ['listing_id', 'id', 'date', 'reviewer_id', 'reviewer_name']
 
 # 3. Checking of the duplicated values
 
 print(calendar_data.duplicated().value_counts())
 print(listings_data.duplicated().value_counts())
-print(neighbourhoods_data.duplicated().value_counts())
-
+print(reviews_data.duplicated().value_counts())
 separator()
 
 # 4. Checking of the value types
@@ -60,7 +59,7 @@ print(calendar_data.dtypes)
 separator()
 print(listings_data.dtypes)
 separator()
-print(neighbourhoods_data.dtypes)
+print(reviews_data.dtypes)
 separator()
 
 # 5. Checking of the missing values
@@ -69,7 +68,7 @@ print(calendar_data.isnull().sum())
 separator()
 print(listings_data.isnull().sum()) 
 separator()
-print(neighbourhoods_data.isnull().sum()) 
+print(reviews_data.isnull().sum()) 
 separator()
 
 # 6. Processing of the missing values
@@ -81,17 +80,14 @@ calendar_data['price'] = calendar_data['price'].str.replace(',', '')
 calendar_data['price'] = calendar_data['price'].astype(float)
 calendar_data['price'] = calendar_data['price'].fillna(calendar_data.price.mean())
 
-calendar_data['adjusted_price'] = calendar_data['adjusted_price'].str.replace('$', '')
-calendar_data['adjusted_price'] = calendar_data['adjusted_price'].str.replace(',', '')
-calendar_data['adjusted_price'] = calendar_data['adjusted_price'].astype(float)
-calendar_data['adjusted_price'] = calendar_data['adjusted_price'].fillna(calendar_data.adjusted_price.mean())
-
 calendar_data['minimum_nights'] = calendar_data['minimum_nights'].fillna(calendar_data.minimum_nights.mean())
 calendar_data['maximum_nights'] = calendar_data['maximum_nights'].fillna(calendar_data.maximum_nights.mean())
 
 # b. listings_data
 
-listings_data['host_location'] = listings_data['host_location'].fillna('Not specified')
+listings_data['host_name'] = listings_data['host_name'].fillna('Not specified')
+listings_data['host_is_superhost'] = listings_data['host_is_superhost'].fillna('Not specified')
+listings_data['host_listings_count'] = listings_data['host_listings_count'].fillna(1)
 listings_data['bathrooms_text'] = listings_data['bathrooms_text'].fillna('Not specified')
 listings_data['bedrooms'] = listings_data['bedrooms'].fillna(method='bfill')
 listings_data['beds'] = listings_data['beds'].fillna(method='bfill')
@@ -113,3 +109,14 @@ listings_data['price'] = listings_data['price'].str.replace(',', '')
 listings_data['price'] = listings_data['price'].astype(float)
 print(listings_data.dtypes)
 separator()
+
+# c. reviews_data
+
+print(reviews_data.dtypes)
+separator()
+
+# 8. Recording of the cleaned data
+
+cleaned_calendar_data = calendar_data.to_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/02_intermediate/cleaned_calendar.csv")
+cleaned_listings_data = listings_data.to_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/02_intermediate/cleaned_listings.csv")
+cleaned_reviews_data = reviews_data.to_csv("/home/apprenant/Documents/Brief-12-Airbnb/data/02_intermediate/cleaned_reviews.csv")
